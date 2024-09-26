bdba0172fcfd31cb7023eb4032e114002cabc884
Ethan-GAO745 <gaojiezhen123@163.com>
2024-09-26 05:24:48 -0700

    Add files via upload

---
+++ study1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int arr[] = {8, 3, 5, 1, 7, 4, 9, 2, 6, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    qsort(arr, n, sizeof(int), compare);
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
